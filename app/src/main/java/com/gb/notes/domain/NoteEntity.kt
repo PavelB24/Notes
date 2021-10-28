ef58dd6e9cd9df16934b0dbe148d6c32986da162
Pasha <alkatrazzzzz@mail.ru>
2021-10-28 20:44:19 +0300

    Переделал note entity.

--- app/src/main/java/com/gb/notes/domain/NoteEntity.kt
+++ app/src/main/java/com/gb/notes/domain/NoteEntity.kt
@@ -1,118 +1,55 @@
-package com.gb.notes.domain;
+package com.gb.notes.domain
 
-import android.os.Parcel;
-import android.os.Parcelable;
+import android.os.Parcelable
+import android.os.Parcel
+import android.os.Parcelable.Creator
+import java.io.Serializable
 
-import java.io.Serializable;
+data class NoteEntity(
+    var id: String,
+    var title: String,
+    var detail: String,
+    var originDay: Int,
+    var originMonth: Int,
+    var originYear: Int
+) : Parcelable, Serializable {
 
-public class NoteEntity implements Parcelable, Serializable {
 
-    private String id;
-    private String title;
-    private String description;
-    private int originDay;
-    private int originMonth;
-    private int originYear;
+    val dateAsString: String
+        get() = "$originDay.$originMonth.$originYear"
 
-    public NoteEntity(String id, String title, String detail, int originDay, int originMonth, int originYear) {
-        this.id = id;
-        this.title = title;
-        this.description = detail;
-        this.originDay = originDay;
-        this.originMonth = originMonth + 1;
-        this.originYear = originYear;
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readInt(),
+        parcel.readInt(),
+        parcel.readInt()
+    ) {
     }
 
-    public int getOriginDay() {
-        return originDay;
+    override fun describeContents(): Int {
+        return 0
     }
 
-    public void setOriginDay(int originDay) {
-        this.originDay = originDay;
+    override fun writeToParcel(parcel: Parcel, i: Int) {
+        parcel.writeString(id)
+        parcel.writeString(title)
+        parcel.writeString(detail)
+        parcel.writeInt(originDay)
+        parcel.writeInt(originMonth)
+        parcel.writeInt(originYear)
     }
 
-    public int getOriginMonth() {
-        return originMonth;
-    }
-
-    public void setOriginMonth(int originMonth) {
-        this.originMonth = originMonth;
-    }
-
-    public int getOriginYear() {
-        return originYear;
-    }
-
-    public void setOriginYear(int originYear) {
-        this.originYear = originYear;
-    }
-
-    private NoteEntity(Parcel in) {
-        id = in.readString();
-        title = in.readString();
-        description = in.readString();
-        originDay= in.readInt();
-        originMonth= in.readInt();
-        originYear= in.readInt();
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-
-    public String getId() {
-        return id;
-    }
-
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDetail() {
-        return description;
-    }
-
-    public void setDetail(String detail) {
-        this.description = detail;
-    }
-
-    public String getDateAsString() {
-        return originDay + "." + originMonth + "." + originYear;
-    }
-
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel parcel, int i) {
-        parcel.writeString(id);
-        parcel.writeString(title);
-        parcel.writeString(description);
-        parcel.writeInt(originDay);
-        parcel.writeInt(originMonth);
-        parcel.writeInt(originYear);
-    }
-
-
-    public static final Parcelable.Creator<NoteEntity> CREATOR = new Parcelable.Creator<NoteEntity>() {
-
-        @Override
-        public NoteEntity createFromParcel(Parcel in) {
-            return new NoteEntity(in);
+    companion object CREATOR : Creator<NoteEntity> {
+        override fun createFromParcel(parcel: Parcel): NoteEntity {
+            return NoteEntity(parcel)
         }
 
-        @Override
-        public NoteEntity[] newArray(int size) {
-            return new NoteEntity[size];
+        override fun newArray(size: Int): Array<NoteEntity?> {
+            return arrayOfNulls(size)
         }
-    };
+    }
+
+
 }
